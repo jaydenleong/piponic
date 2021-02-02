0bb9a712f11cf91d0711160c89bc5fe01a51d2c6
cbrrrry <carsonberry@hotmail.ca>
2021-02-02 09:05:37 +0000

    Testing ADC code with leak sensor - status unknown

---
+++ adc_read.py
@@ -0,0 +1,9 @@
+'''
+test read for leak sensor
+
+
+'''
+import mcp3008
+
+with mcp3008.MCP3008() as adc:
+    print adc.read([mcp3008.CH0]) #prints raw data [ch0]
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# File: setup.bash
+#
+# Author: Carson Berry, following form of Jayden Leong
+#
+# Date: Feb 2, 2021
+#
+# Purpose: install dependencies for piponic project on Raspberry Pi Zero W V1.1
+#
+# Usage: sudo ./setup.bash
+
+pip3 install -r requirements.txt
