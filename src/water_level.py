95a3d38f3e139f552d9383cb6e61d2e8a0c4d318
cbrrrry <carsonberry@hotmail.ca>
2021-02-09 23:16:46 -0800

    Wrote Water Level code with no filtering

---
+++ src/water_level.py
@@ -0,0 +1,39 @@
+'''
+water_level.py 
+
+Author: Carson Berry
+Date: February 9th, 2021
+
+A function that reads from the SEN0204 non-contact liquid level sensor. Available from https://www.dfrobot.com/product-1493.html. 
+
+
+Output:
+    water_level object (0 is no water, 1 is water)
+
+Usage: 
+    import src.water_level as WL
+    WL_sensor = WL.water_level()
+    print(WL_sensor.read())
+
+'''
+
+
+import RPi.GPIO as GPIO
+import src.pins as pins
+
+
+class water_level(object):
+    def __init__(self):
+        self.level = 0
+        self.setup()
+        self.read() # update level 
+
+    def setup(self):
+        GPIO.setmode(GPIO.BCM) #read GPIO labels not pin numbers 
+
+        #Enable input, pull-down resistor so GPIO pin is normally GND
+        GPIO.setup(pins.WATER_LEVEL,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    def read(self):
+        self.level = GPIO.input(pins.WATER_LEVEL)
+        return self.level
