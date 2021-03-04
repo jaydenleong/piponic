a9bbfc86eafeb49ed331ef05b21dfae65f21e874
Han Xu <43010521+limbotempo@users.noreply.github.com>
2021-03-03 21:36:05 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -16,7 +16,7 @@ class Hello5Program:
         while self._running:
             time.sleep(5) #Five second delay
             cycle = cycle + 1.0
-            print "5 Second Thread cycle+1.0 - ", cycle
+            print ('5 Second Thread cycle+1.0 - ')
 
 class Hello2Program:  
     def __init__(self):
@@ -30,7 +30,7 @@ class Hello2Program:
         while self._running:
             time.sleep(2) #Five second delay
             cycle = cycle + 0.5
-            print "5 Second Thread cycle+1.0 - ", cycle
+            print ('5 Second Thread cycle+1.0 - ')
 
 #Create Class
 FiveSecond = Hello5Program()
@@ -46,14 +46,3 @@ TwoSecondThread = Thread(target=TwoSecond.run)
 #Start Thread 
 TwoSecondThread.start()
 
-
-Exit = False #Exit flag
-while Exit==False:
- cycle = cycle + 0.1 
- print "Main Program increases cycle+0.1 - ", cycle
- time.sleep(1) #One second delay
- if (cycle > 5): Exit = True #Exit Program
-
-TwoSecond.terminate()
-FiveSecond.terminate()
-print "Goodbye :)"
